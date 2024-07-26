eac2b4306a8e71286891463248b91240425ea8f2
Daniel Lobaton <Dlobaton2@gmail.com>
2024-07-26 14:05:15 -0700

    Create beep.rb

---
+++ beep.rb
@@ -0,0 +1,15 @@
+class Beep < Formula
+  desc "CLI tool to play beep sounds and respond to beep commands on macOS"
+  homepage "https://github.com/G2X-Team/beep"
+  url "https://github.com/G2X-Team/beep.git", branch: "main"
+  version "1.0.0"
+  license "MIT"
+
+  def install
+    bin.install "beep"
+  end
+
+  test do
+    assert_match "beep beep!", shell_output("#{bin}/beep")
+  end
+end
